6b2af1cc9a89e921051733177eb58f1b1b858b70
Fred Jonsson <fridrik@pyth.net>
2012-07-20 22:13:10 +0000

    Add request timing.

--- debug_toolbar_requests/panel.py
+++ debug_toolbar_requests/panel.py
@@ -1,4 +1,6 @@
+from functools import partial
 from threading import local
+import time
 
 import requests
 
@@ -6,6 +8,18 @@ from django.utils.translation import ugettext_lazy as _, ngettext
 
 from debug_toolbar.panels import DebugPanel
 
+class ResponseTimer(object):
+    def __init__(self, start_time=None, end_time=None, response=None):
+        self.start_time = start_time
+        self.end_time = end_time
+        self.response = response
+
+    @property
+    def duration(self):
+        return self.end_time - self.start_time
+
+# Retain, because it won't be retrievable after monkey-patching.
+original_thread_class = requests.models.Request
 
 class RequestsDebugPanel(DebugPanel):
     """
@@ -16,20 +30,34 @@ class RequestsDebugPanel(DebugPanel):
     template = 'debug_toolbar/panels/requests.html'
     has_content = True
 
-    def set_response(self, response):
-        self.thread_locals.responses.append(response)
+    def receive_response(self, index, response):
+        print 'response', index
+        self.thread_locals.response_timers[index].end_time = time.time()
+        self.thread_locals.response_timers[index].response = response
+
+    def receive_request(self, index, request):
+        print 'request', index
+        self.thread_locals.response_timers[index].start_time = time.time()
 
     def __init__(self, *args, **kwargs):
         super(RequestsDebugPanel, self).__init__(*args, **kwargs)
 
         self.thread_locals = local()
-        self.thread_locals.responses = []
+        self.thread_locals.response_timers = []
         debug_panel = self
 
-        class TrackedRequest(requests.models.Request):
+        class TrackedRequest(original_thread_class):
             def __init__(self, *args, **kwargs):
                 super(TrackedRequest, self).__init__(*args, **kwargs)
-                self.register_hook('response', debug_panel.set_response)
+
+                response_timer = ResponseTimer()
+                next_index = len(debug_panel.thread_locals.response_timers)
+                debug_panel.thread_locals.response_timers.append(response_timer)
+
+                self.register_hook('pre_request',
+                    hook=partial(debug_panel.receive_request, next_index))
+                self.register_hook('response',
+                    hook=partial(debug_panel.receive_response, next_index))
 
         # TODO: in the interest of forward-compatibility, can this be done
         #   more safely dynamically; e.g. by looking for use of the `Request`
@@ -45,7 +73,7 @@ class RequestsDebugPanel(DebugPanel):
         return _('HTTP Requests')
 
     def nav_subtitle(self):
-        request_count = len(self.thread_locals.responses)
+        request_count = len(self.thread_locals.response_timers)
         return ngettext("%d request", "%d requests", request_count) % request_count
 
     def url(self):
@@ -53,6 +81,6 @@ class RequestsDebugPanel(DebugPanel):
 
     def process_response(self, request, response):
         self.record_stats({
-            'responses': self.thread_locals.responses,
+            'response_timers': self.thread_locals.response_timers,
         })
 
